773aaec5e0414f512dd6b4cb69587f20165ea4f3
功夢 佐藤 <k022g0117@g.neec.ac.jp>
2023-05-15 17:40:49 +0900

    Windowクラス

--- WinApp.cpp
+++ WinApp.cpp
@@ -1,1 +1,71 @@
 #include "WinApp.h"
+
+//ウィンドウプロシージャ
+LRESULT CALLBACK WinApp::WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+    //メッセージに応じてゲーム固有の処理を行う
+    switch (msg) {
+        //ウィンドウが破棄された
+    case WM_DESTROY:
+        // OSに対して、アプリの終了を伝える
+        PostQuitMessage(0);
+        return 0;
+    }
+    // 標準のメッセージ処理を行う
+    return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+void WinApp::WindowView() {
+    //ウィンドウプロシージャ
+    wc.lpfnWndProc = WindowProc;
+    //クラス名
+    wc.lpszClassName = L"CG2WindowClass";
+    //インスタンスハンドル
+    wc.hInstance = GetModuleHandle(nullptr);
+    //カーソル
+    wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+
+    //ウィンドウクラス登録
+    RegisterClass(&wc);
+
+    //ウィンドウサイズの構造体にクライアント領域を入れる
+    RECT wrc = { 0,0,kClientWidth,kClientHeight };
+
+    //クライアント領域を元に実際のサイズにwrcを変更してもらう
+    AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+    //ウィンドウの生成
+    hwnd = CreateWindow(
+        wc.lpszClassName,//クラス名
+        L"CG2",//タイトルバーの名前
+        WS_OVERLAPPEDWINDOW,//ウィンドウスタイル
+        CW_USEDEFAULT,//表示X座標
+        CW_USEDEFAULT,//表示Y座標
+        wrc.right - wrc.left,//ウィンドウ横幅
+        wrc.bottom - wrc.top,//ウィンドウ縦幅
+        nullptr,//親ウィンドウハンドル
+        nullptr,//メニューハンドル
+        wc.hInstance,//インスタンスハンドル
+        nullptr//オプション
+    );
+
+    //ウィンドウ表示
+    ShowWindow(hwnd, SW_SHOW);
+}
+
+int WinApp::ProccessMessage() {
+    MSG msg;
+
+    //Windowのメッセージを最優先で処理させる
+    if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+        TranslateMessage(&msg);
+        DispatchMessage(&msg);
+    }
+
+    if (msg.message == WM_QUIT) {
+        return true;
+    }
+
+    return false;
+}
+
+HWND WinApp::hwnd;
