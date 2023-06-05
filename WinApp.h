7549e637a49702ab49fd5529a430eccf9318c41c
功夢 佐藤 <k022g0117@g.neec.ac.jp>
2023-06-05 14:12:45 +0900

    未完成

--- WinApp.h
+++ WinApp.h
@@ -2,11 +2,19 @@
 #include <Windows.h>
 #include <cstdint>
 
-class WinApp
-{
+class WinApp {
 public:
-	// クライアント領域のサイズ
-	const int32_t kClientWidth = 1280;
-	const int32_t kClientHeight = 720;
-};
+    //クライアント領域サイズ
+    static const int32_t kClientWidth = 1280;
+    static const int32_t kClientHeight = 720;
 
+    static LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam);
+
+    static void WindowView();
+
+    static int ProccessMessage();
+
+private:
+    static inline WNDCLASS wc{};// ウィンドウクラス
+    static HWND hwnd;
+};
